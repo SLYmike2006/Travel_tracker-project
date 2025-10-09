4ee606f8906d8c47aebaaf279c4fd6d79c6937a4
Michael Yang <michaelslyang@gmail.com>
2025-10-09 00:00:04 -0500

    added back to home page button

--- init_db.py
+++ init_db.py
@@ -1,6 +1,6 @@
-from app import app, db
+from app import app, db, User, Place  
 
 with app.app_context():
     db.create_all()
 
-print("Database initialized.")
+print("✅ Database initialized successfully.")
